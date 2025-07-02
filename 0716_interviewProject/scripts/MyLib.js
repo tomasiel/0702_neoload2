e87e616c8c3e05dec5fa469effd505394b900afa
tomasiel <ltomasiello2024@gmail.com>
2025-07-02 09:55:12 -0400

    first commit

---
+++ 0716_interviewProject/scripts/MyLib.js
@@ -0,0 +1,21 @@
+﻿function writeFile(text)
+
+{
+var lock = new java.util.concurrent.locks.ReentrantLock();
+var date = context.variableManager.getValue("CurrentDate");
+
+lock.lock();
+                                             
+var writer = new java.io.FileWriter("C:\\NeoLoad\\OutputFile\\TestFile2.csv",true);
+
+//writer.write(date+";"+context.currentVU.id+";"+ text );
+
+writer.write(date+","+context.currentVU.id+","+text );
+
+writer.write("rn");
+
+writer.close();
+
+lock.unlock();
+
+}
---
+++ 0716_interviewProject/scripts/jsAction_6e29125c-61eb-4939-b275-40de7601636e.js
@@ -0,0 +1,15 @@
+﻿// Get today's date
+var today = new Date();
+
+// Increment by one day
+today.setDate(today.getDate() + 1);
+
+// Format the new date as YYYY-MM-DD
+var year = today.getFullYear();
+var month = ('0' + (today.getMonth() + 1)).slice(-2); // Month is zero-based
+var day = ('0' + today.getDate()).slice(-2);
+
+var formattedDate = year + '-' + month + '-' + day;
+
+// Store the date in a NeoLoad variable
+vars.put("nextDay", formattedDate);
